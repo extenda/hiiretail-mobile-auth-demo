78dc2d4a46bc72d098a2d80f549d124f50698aea
Dmitriy Stadnik <dmitriy.stadnyk@extendaretail.com>
2021-06-23 18:53:57 +0300

    Pass firebase config via postMessage API

--- App.tsx
+++ App.tsx
@@ -1,9 +1,8 @@
 import React, {useRef} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-import { WebView } from 'react-native-webview';
+import {StyleSheet, Text, View} from 'react-native';
+import {WebView} from 'react-native-webview';
 
-const tenantId = 'testrunner-2mfuk';
-const providerId = 'oidc.wbchz';
+const tenantAlias = 'extenda';
 const OAUTH_USER_AGENT = 'Mozilla/5.0 (Linux; Android;) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/43.0.2357.65 Mobile Safari/537.36';
 
 
@@ -24,45 +23,48 @@ export default function App() {
     const webViewRef = useRef<WebView>(null);
     const initRef = useRef(false);
     const firebaseConfig = {
-        apiKey: 'AIzaSyAmzj7dLHB1U524laWK0e-zIFl-U6Ay9uo',
-        authDomain: 'hiidentity-staff-staging.firebaseapp.com',
-        databaseURL: 'https://hiidentity-staff-staging.firebaseio.com',
-        projectId: 'hiidentity-staff-staging',
-        storageBucket: 'hiidentity-staff-staging.appspot.com',
-        messagingSenderId: '448695078080',
-        appId: '1:448695078080:web:84adb8e2acaac139d87125'
+        apiKey: 'AIzaSyBoPRi3VkcBOgRPWyoaVyUYcbSZG13Cmiw',
+        authDomain: 'hiidentity-staff.firebaseapp.com',
+        databaseURL: 'https://hiidentity-staff.firebaseio.com',
+        projectId: 'hiidentity-staff',
+        storageBucket: 'hiidentity-staff.appspot.com',
+        messagingSenderId: '507969065515',
+        appId: '1:507969065515:web:74378c7d42cd5838b7906e',
     };
-    const INJECTED_JAVASCRIPT = `init(${JSON.stringify(firebaseConfig)});`
+    // const INJECTED_JAVASCRIPT = `init(${JSON.stringify(firebaseConfig)}, '${tenantAlias}');`;
+    console.log(`${JSON.stringify({type: 'INIT', data: {firebaseConfig, clientId: 'test'}})}`);
     const onWarning = (e: any) => {
         alert(e);
         console.log(e);
     }
     return (
-    <View style={styles.container}>
-        <Text>Login</Text>
-      <WebView
-          ref={webViewRef}
-          userAgent={OAUTH_USER_AGENT}
-          style={{ marginTop: 20, width: 400 }}
-          source={{uri: 'http://extenda.hiiretail.com:8080/'}}
-          javaScriptEnabled
-          domStorageEnabled
-          startInLoadingState
-          onMessage={(event) => {
+        <View style={styles.container}>
+            <Text>Login</Text>
+            <WebView
+                ref={webViewRef}
+                userAgent={OAUTH_USER_AGENT}
+                style={{marginTop: 20, width: 400}}
+                source={{uri: 'http://extenda.hiiretail.com:9000/login?client=mobile'}}
+                javaScriptEnabled
+                domStorageEnabled
+                onMessage={(event) => {
               // const message = JSON.parse(event.nativeEvent.data);
               // alert(event.nativeEvent.data);
               console.log(event.nativeEvent.data);
           }}
-          onLoadEnd={() => {
-              webViewRef.current?.injectJavaScript(INJECTED_JAVASCRIPT);
-              webViewRef.current?.injectJavaScript('getRedirectResult()');
-              if (!initRef.current) {
-                  initRef.current = true;
-              }
+                onLoadEnd={() => {
+                    if (webViewRef.current) {
+                        setTimeout(() => {
+                            webViewRef.current?.postMessage(`${JSON.stringify({
+                                type: 'INIT',
+                                data: {firebaseConfig, clientId: 'test'}
+                            })}`);
+                        }, 100)
+                    }
+                }
           }
-          }
-          onError={onWarning}
-          onHttpError={onWarning}
+                onError={onWarning}
+                onHttpError={onWarning}
       />
     </View>
   );
