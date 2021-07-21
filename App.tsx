d8fb4f4e00bd8db3673e2c5f9aeb4272b33313ab
Dmitriy Stadnik <dmitriy.stadnyk@extendaretail.com>
2021-07-21 08:33:40 +0300

    Add demo code

--- App.tsx
+++ App.tsx
@@ -2,15 +2,7 @@ import React, {useRef} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import {WebView} from 'react-native-webview';
 
-const tenantAlias = 'extenda';
 const OAUTH_USER_AGENT = 'Mozilla/5.0 (Linux; Android;) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/43.0.2357.65 Mobile Safari/537.36';
-
-
-type Message = {
-    type: string;
-    data: Record<string, unknown>
-}
-
 const styles = StyleSheet.create({
     container: {
         flex: 1,
@@ -21,20 +13,12 @@ const styles = StyleSheet.create({
 });
 export default function App() {
     const webViewRef = useRef<WebView>(null);
-    const initRef = useRef(false);
     const firebaseConfig = {
-        apiKey: 'AIzaSyBoPRi3VkcBOgRPWyoaVyUYcbSZG13Cmiw',
-        authDomain: 'hiidentity-staff.firebaseapp.com',
-        databaseURL: 'https://hiidentity-staff.firebaseio.com',
-        projectId: 'hiidentity-staff',
-        storageBucket: 'hiidentity-staff.appspot.com',
-        messagingSenderId: '507969065515',
-        appId: '1:507969065515:web:74378c7d42cd5838b7906e',
+        apiKey: "AIzaSyBn2akUn5Iq9wLfVwPUsHiTtSP7EV2k-FU",
+        authDomain: "hiidentity-staff.firebaseapp.com",
+
     };
-    // const INJECTED_JAVASCRIPT = `init(${JSON.stringify(firebaseConfig)}, '${tenantAlias}');`;
-    console.log(`${JSON.stringify({type: 'INIT', data: {firebaseConfig, clientId: 'test'}})}`);
     const onWarning = (e: any) => {
-        alert(e);
         console.log(e);
     }
     return (
@@ -44,29 +28,15 @@ export default function App() {
                 ref={webViewRef}
                 userAgent={OAUTH_USER_AGENT}
                 style={{marginTop: 20, width: 400}}
-                source={{uri: 'http://extenda.hiiretail.com:9000/login?client=mobile'}}
+                source={{uri: `https://testrunner.hiiretail.com/login?client=mobile&apiKey=${firebaseConfig.apiKey}&authDomain=${firebaseConfig.authDomain}`}}
                 javaScriptEnabled
                 domStorageEnabled
                 onMessage={(event) => {
-              // const message = JSON.parse(event.nativeEvent.data);
-              // alert(event.nativeEvent.data);
-              console.log(event.nativeEvent.data);
-          }}
-                onLoadEnd={() => {
-                    if (webViewRef.current) {
-                        setTimeout(() => {
-                            webViewRef.current?.postMessage(`${JSON.stringify({
-                                type: 'INIT',
-                                data: {firebaseConfig, clientId: 'test'}
-                            })}`);
-                        }, 100)
-                    }
-                }
-          }
+                    console.log(event.nativeEvent.data)
+                }}
                 onError={onWarning}
                 onHttpError={onWarning}
       />
     </View>
   );
-};
-
+}
